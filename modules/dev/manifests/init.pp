2aed7ba6445b4e347f247d585b0bf9ee994dcf94
Will Maier <willmaier@ml1.net>
2010-11-18 09:45:55 -0600

    Installation works now, too.

--- modules/dev/manifests/init.pp
+++ modules/dev/manifests/init.pp
@@ -3,4 +3,6 @@ class dev {
 	package { "python->=2.5,<2.6": alias => "python2.5" }
 	package { "python->=2.6,<2.7": alias => "python2.6" }
 	package { "vim-*-no_x11": ensure => present }
+	package { "mercurial-*": ensure => present }
+	package { "nhc98-*": ensure => present }
 }

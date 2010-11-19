394a268e82539db5a6ae251034c08e24ccc42c8d
Will Maier <willmaier@ml1.net>
2010-11-18 18:42:17 -0600

    Note puppet-openbsd requirement.

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,6 +6,9 @@ filebucket { "main": server => "puppet.lfod.us" }
 
 File { backup => main }
 Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin" }
+
+# This configuration requires the puppet-openbsd module:
+#    http://github.com/wcmaier/puppet-openbsd
 Package { 
 	provider => "openbsd",
 	source   => "http://mirror.lfod.us/pub/OpenBSD/snapshots/packages/${architecture}/",

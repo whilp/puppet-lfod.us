c6e683d4fddcdb8da01d739e0c53961540bd355f
Will Maier <willmaier@ml1.net>
2010-11-18 18:02:53 -0600

    Add uninstall.

--- modules/dev/manifests/init.pp
+++ modules/dev/manifests/init.pp
@@ -4,9 +4,9 @@ class dev {
 	package { "python->=2.5,<2.6": alias => "python2.5" }
 	package { "python-bsddb->=2.5,<2.6": alias => "python2.5-bsddb" }
 	package { "python->=2.6,<2.7": alias => "python2.6" }
-	#package { "python-bsddb->=2.6,<2.7": alias => "python2.6-bsddb" }
+	package { "python-bsddb->=2.6,<2.7": alias => "python2.6-bsddb" }
 	package { "vim-*-no_x11": ensure => present }
 	package { "mercurial-*": ensure => present }
-	package { "nhc98-*": ensure => present }
+	package { "nhc98-*": ensure => absent }
 	package { "ghc-*": ensure => present }
 }
